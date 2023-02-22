0a622f243a41a87bfbfdc92ee2e2b3a6a37d53e0
Remus Courtenay <remus.courtenay@servian.com>
2023-02-22 13:32:03 +1300

    hello world

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "remuspersonalwebsitef097a1ca": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "remuspersonalwebsitestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
