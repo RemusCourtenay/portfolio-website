70223f16141a25a316b0c9872ac7043d20cadd15
Remus Courtenay <remus.courtenay@servian.com>
2023-03-31 13:17:16 +1300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import '@aws-amplify/ui-react/styles.css';
+import '@fontsource/inter/variable.css';
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './ui-components';
+ 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello Mum
         </p>
         <a
           className="App-link"
---
+++ src/ui-components/NavBar.d.ts
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+import { ButtonProps, FlexProps, IconProps, ImageProps, SearchFieldProps, TextProps, ViewProps } from "@aws-amplify/ui-react";
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type NavBarOverridesProps = {
+    NavBar?: PrimitiveOverrideProps<FlexProps>;
+    Logo29767073?: PrimitiveOverrideProps<FlexProps>;
+    "Star 1"?: PrimitiveOverrideProps<IconProps>;
+    Logo29767075?: PrimitiveOverrideProps<TextProps>;
+    "Frame 32129767076"?: PrimitiveOverrideProps<FlexProps>;
+    Button36482689?: PrimitiveOverrideProps<ButtonProps>;
+    Button36482714?: PrimitiveOverrideProps<ButtonProps>;
+    Button36482718?: PrimitiveOverrideProps<ButtonProps>;
+    Button36482722?: PrimitiveOverrideProps<ButtonProps>;
+    "Frame 32129767081"?: PrimitiveOverrideProps<FlexProps>;
+    SearchField?: PrimitiveOverrideProps<SearchFieldProps>;
+    Icon?: PrimitiveOverrideProps<ViewProps>;
+    Vector?: PrimitiveOverrideProps<IconProps>;
+    image?: PrimitiveOverrideProps<ImageProps>;
+} & EscapeHatchProps;
+export declare type NavBarProps = React.PropsWithChildren<Partial<FlexProps> & {
+    overrides?: NavBarOverridesProps | undefined | null;
+}>;
+export default function NavBar(props: NavBarProps): React.ReactElement;
