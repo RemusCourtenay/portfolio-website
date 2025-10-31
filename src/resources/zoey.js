b709fe2b9f030e499c250cd6e7ce1e1c6fc6cdc0
ZoeyStardream <zoeystardream@gmail.com>
2025-10-31 17:31:08 +1300

    project names

--- src/resources/zoey.js
+++ src/resources/zoey.js
@@ -12,26 +12,26 @@ export const resources = {
     },
     SKILLS: [
         {   
-            SKILL_NAME: "Responsive Web App Development",
+            SKILL_NAME: "Responsive Web\nApp Development",
             FOCUS_VALUES: {
                 PERSONAL: false,
-                EDUCATION: true,
+                EDUCATIONAL: true,
                 COMMERCIAL: true
             }
         },
         {   
-            SKILL_NAME: "Database Connected Backends",
+            SKILL_NAME: "Database Connected\nBackends",
             FOCUS_VALUES: {
                 PERSONAL: false,
-                EDUCATION: true,
+                EDUCATIONAL: true,
                 COMMERCIAL: true
             }
         },
         {   
-            SKILL_NAME: "Videogame Testing and Debugging",
+            SKILL_NAME: "Videogame Testing\nAnd Debugging",
             FOCUS_VALUES: {
                 PERSONAL: true,
-                EDUCATION: false,
+                EDUCATIONAL: false,
                 COMMERCIAL: false
             }
         },
@@ -39,7 +39,7 @@ export const resources = {
             SKILL_NAME: "Data Reporting",
             FOCUS_VALUES: {
                 PERSONAL: false,
-                EDUCATION: true,
+                EDUCATIONAL: true,
                 COMMERCIAL: true
             }
         },
@@ -47,7 +47,7 @@ export const resources = {
             SKILL_NAME: "Task Automation",
             FOCUS_VALUES: {
                 PERSONAL: true,
-                EDUCATION: true,
+                EDUCATIONAL: true,
                 COMMERCIAL: false
             }
         }
@@ -72,21 +72,21 @@ export const resources = {
         },
         {
             Name: "Kibo",
-            ExpandedViewTitle: "NASA's Kibo\nRobot Challenge",
-            ExpandedViewBodyText: "Our team embarked on an incredible journey that led us to the international finals of the esteemed Kibo Robot Programming Challenge, following our remarkable victory in the national round held in New Zealand. This prestigious competition, organized by JAXA (Japanese Aerospace Exploration Agency) in collaboration with NASA's Kibo Robot API, showcased numerous teams with exceptional talent and expertise in robotics and programming.\n\nAs the lead Java programmer within our team of ten, I spearheaded the development of innovative solutions to tackle the diverse challenges presented in the international finals. One of my key responsibilities was implementing advanced techniques for AR code and QR code recognition, enabling our robot to intelligently interact with real-world objects and information. Additionally, I led the design and implementation efforts for our robot's 3D movement and location calculations.\n\nAlthough we didn't secure the top spot, I take immense pride in our accomplishment, knowing that we stood among the elite teams from around the world.",
-            CircleViewTitle: "Kibo Robot\nChallenge"
+            ExpandedViewTitle: "Permission Modernization",
+            ExpandedViewBodyText: "",
+            CircleViewTitle: "Permission Modernization"
         },
         {
             Name: "AWS",
-            ExpandedViewTitle: "AI/MLOps\nat Cognizant",
-            ExpandedViewBodyText: "During my internship at Cognizant, I had the opportunity to work on a fascinating project that involved creating Infrastructure-as-Code using Terraform for a machine learning (ML) project hosted on AWS. Utilizing customer data from Cognizant's client Entrada, we successfully deployed a revenue optimization model for their 'Flexi-ticket' product.\n\nTo ensure data security, we implemented a private network with a Virtual Private Cloud (VPC), Network Firewall, and JWT authentication. Throughout the project, we explored the entire ML lifecycle. This involved model development, testing, training, and deployment using AWS SageMaker. To enhance the user experience, I leveraged my skills in web development to create a sleek and interactive web frontend using React, Amplify, and Figma.\n\nThis internship experience provided me with invaluable insights into the practical application of AI and ML in real-world scenarios. I honed my skills in cloud computing, ML operations, and frontend development. It was a rewarding journey that further solidified my passion for AI engineering and showcased my ability to deliver impactful solutions.",
-            CircleViewTitle: "AI/MLOps\nat Cognizant"
+            ExpandedViewTitle: "Data Management\nat Tasman Council",
+            ExpandedViewBodyText: "",
+            CircleViewTitle: "Council Data Management"
         },
         {
             Name: "Honours",
-            ExpandedViewTitle: "Modelling\nSocial Networks",
-            ExpandedViewBodyText: "For the Web Enabled Simulation (WES) project, our aim was to create an open-source replica of Meta's innovative 'WW System' simulation software, to facilitate the study of social networking services. Unlike traditional simulations that rely on creating a model of reality, WES leverages a real-world software platform to simulate the behavior of a user community. In this multi-agent-based approach, each agent acts as a bot, emulating user actions and interactions.\n\nWe developed the program code for this open-source replica in Ruby using the open-source social network Mastodon as our platform. Our goal was to democratize access to the WES system, fostering collaboration and innovation within the research community.\n\nThis project not only honed our skills in software development, particularly in the context of Web-Enabled Simulation, but also deepened our understanding of machine learning applications. Collaborating closely with a partner, we successfully delivered an open-source solution that empowers researchers to study and analyze social network behavior in a controlled and isolated environment.",
-            CircleViewTitle: "Agent-Based Modelling"
+            ExpandedViewTitle: "Game Testing\nand Debugging",
+            ExpandedViewBodyText: "",
+            CircleViewTitle: "Reborn \nGame Testing"
         }
     ],
     CONTACT:{
