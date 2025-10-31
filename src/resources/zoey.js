bfe8e62c767cff872d5c13cec5eb3614f43090ba
ZoeyStardream <zoeystardream@gmail.com>
2025-10-31 17:01:59 +1300

    blurbs

--- src/resources/zoey.js
+++ src/resources/zoey.js
@@ -6,17 +6,17 @@ export const resources = {
             FILE_URI: "Zoey Arnold CV 2025.pdf",
             FILE_NAME: "Zoey Arnold CV 2025.pdf"
         },
-        SPLASH_BLURB: "pls gib job",
-        SKILLS_BLURB: "honestly probably a bad idea",
+        SPLASH_BLURB: "I'm a Software Engineering graduate from the University of Canterbury currently temporarily based in the Nelson region, looking for an entry level position to properly kickstart my career. I'm eager to continue to learn and grow my practical skillset, and see my work make a real difference in the world.",
+        SKILLS_BLURB: "As a qualified junior software engineer, I have experienced the fundamentals of a wide variety of technologies and strategies to create a strong baseline understanding of the concepts and ways of thinking required to be effective in this field without my skillset becoming rigid. Now, I have the desire to deepen my understanding and flexibility to apply myself to whatever new challenges I face, allowing me to learn rapidly. \nWith my aptitude for problem-solving I wish to help create efficient and elegant solutions that accomodate the wants and needs of the parties involved, understanding them through my receptive and empathetic communication style. I believe I can bring a calm and diverse viewpoint to any team and am eager help build the projects of the future.",
         FACE_IMAGE: "assets/bee.png"
     },
     SKILLS: [
         {   
-            SKILL_NAME: "Python",
+            SKILL_NAME: "Responsive Web App Development",
             FOCUS_VALUES: {
                 PERSONAL: false,
-                EDUCATION: false,
-                COMMERCIAL: false
+                EDUCATION: true,
+                COMMERCIAL: true
             }
         },
         {   
